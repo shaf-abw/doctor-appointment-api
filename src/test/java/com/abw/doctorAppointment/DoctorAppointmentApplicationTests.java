5b60e389f97154fdae2f2cf6d6add0a6930190a2
Shafeeque Ahamed <shafeeq.ease@gmail.com>
2024-10-11 14:52:23 +0530

    Initial Commit

---
+++ src/test/java/com/abw/doctorAppointment/DoctorAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abw.doctorAppointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorAppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
